294bbac3525d7ffb6ff2bef34c84f0b8e655d1d8
zenglingji <zenglingji@joyy.sg>
2021-10-15 16:33:00 +0800

    refactor:  替换 LRU 缓存的实现

--- packages/helpers/src/lru-cache.ts
+++ packages/helpers/src/lru-cache.ts
@@ -1,113 +1,130 @@
-class DoubleLinkedListNode {
-  pre: null | DoubleLinkedListNode;
-  next: null | DoubleLinkedListNode;
-  val: any;
-  key: any;
-  constructor(val: any) {
-    this.pre = null;
-    this.next = null;
-    this.val = val
-  }
-}
-
-class LRUCache {
-  private _count: number;
-  private _capacity: number;
-  private _valueMap: Record<string, any>;
-  private _first: any;
-  private _last: any;
-  /**
-   * @param {number} capacity
-   */
+/**
+ * @param {number} capacity
+ */
+ class LRUCache {
+  public map: Map<number, any>;
+  public list: DoubleLinkedList;
+  public capacity: number;
   constructor(capacity: number) {
-    this._count = 0;
-    this._capacity = capacity;
-    this._valueMap = {};
-    this._first = null;
-    this._last = null;
+    this.map = new Map();
+    this.list = new DoubleLinkedList();
+    this.capacity = capacity;
   }
 
-  get(key: string) {
-    const node = this._valueMap[key];
-    const keyExists = !!node;
-
-    if (!keyExists) {
-      this._valueMap[key] = void 0;
+  /** 
+  * @param {number} key
+  * @return {number}
+  */
+  get(key: number): number {
+    if (!this.map.has(key)) {
       return -1;
     }
 
-    this._setMRU(key);
+    const node = this.map.get(key);
+
+    this.list.splice(node);
+    const newNode = this.list.push(key, node.val);
+    this.map.set(key, newNode);
     return node.val;
   }
 
-  put(key: string, value: any) {
-    const node = this._valueMap[key];
-    const keyExists = !!node;
-  
-    if (keyExists) {
-      node.val = value;
-    } else {
-      if (this._count >= this._capacity) {
-        this._removeLRU();
+  /** 
+  * @param {number} key 
+  * @param {number} value
+  * @return {void}
+  */
+  put(key: number, value: number): void {
+    if (!this.map.has(key)) {
+      const node = this.list.push(key, value);
+      this.map.set(key, node);
+      if (this.list.count > this.capacity) {
+        const lruNode = this.list.shift();
+        if (lruNode) {
+          this.map.delete(lruNode.key);
+        }
       }
-      this._addToList(key, value);
-    }
-    this._setMRU(key);
-  }
-
-  private _removeLRU() {
-    const second = this._first.next;
-    const key = this._first.key;
-    this._first.next = null;
-    if (second) {
-      second.pre = null; 
-    }
-    this._first = second;
-    this._count --;
-    delete this._valueMap[key];
-  }
-
-  private _setMRU(key: string) {
-    const newLast = this._valueMap[key];
-    const preLast = this._last;
-
-    if (preLast === newLast) {
       return;
     }
 
-    this._last = newLast;
+    this.list.splice(this.map.get(key));
+    const node = this.list.push(key, value);
+    this.map.set(key, node);
+  }
+}
 
-    // If newLast.pre is falsy, this._first should be updated because it means newLast was the first node.
-    if (newLast.pre) {
-      newLast.pre.next = newLast.next;
-    } else if (newLast.next) {
-      this._first = this._first.next;
-    }
-    if (newLast.next) {
-      newLast.next.pre = newLast.pre; 
-    }
-    if (preLast) {
-      preLast.next = newLast;
-      newLast.pre = preLast;
-      newLast.next = null;
-    }
+/**
+ * Your LRUCache object will be instantiated and called as such:
+ * var obj = new LRUCache(capacity)
+ * var param_1 = obj.get(key)
+ * obj.put(key,value)
+ */
+
+class DoubleLinkedListNode {
+  public pre: DoubleLinkedListNode | null;
+  public next: DoubleLinkedListNode | null;
+  public val: any;
+  public key: any;
+  constructor (val?: any, key: number | null = null) {
+    this.pre = null;
+    this.next = null;
+    this.val = val;
+    this.key = key;
+  }
+}
+
+class DoubleLinkedList {
+  public head: DoubleLinkedListNode;
+  public tail: DoubleLinkedListNode;
+  public count: number;
+  constructor () {
+    this.head = new DoubleLinkedListNode();
+    this.tail = new DoubleLinkedListNode();
+    this.head.next = this.tail;
+    this.tail.pre = this.head;
+    this.count = 0;
   }
 
-  private _addToList(key: string, value: any) {
-    this._count ++;
+  push(key: number, val: number) {
+    const node = new DoubleLinkedListNode(val, key);
+    const last = this.tail.pre!;
+    last.next = node;
+    node.pre = last;
+    this.tail.pre = node;
+    node.next = this.tail;
+    this.count ++;
 
-    const newLast = new DoubleLinkedListNode(value);
+    return node;
+  }
 
-    // the key property is for deleting the LRU Node in the map within the _removeLRU method.
-    newLast.key = key;
-    this._valueMap[key] = newLast;
 
-    // If this._first is falsy, directly set the newLast to this._first and this._last.
-    if (!this._first) {
-      this._first = this._last = newLast;
-    } else {
-      this._setMRU(key);
+  shift(): DoubleLinkedListNode | null {
+    if (this.count < 1) {
+      return null;
     }
+
+    const first = this.head.next!;
+
+    this.head.next = first.next;
+    first.next!.pre = this.head;
+    first.next = null;
+    first.pre = null;
+    this.count --;
+    return first;
+  }
+
+  splice(node: DoubleLinkedListNode) {
+    if (this.count < 1) {
+      return null;
+    }
+
+    const pre = node.pre!;
+    const next = node.next!;
+    node.pre = null;
+    node.next = null;
+    pre.next = next;
+    next.pre = pre;
+    this.count --;
+    return node;
   }
 }
 
