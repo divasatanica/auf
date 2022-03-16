467c20a073cfe98bce7ae66210f810fb542ccc5d
zenglingji <zenglingji@joyy.com>
2022-03-16 17:55:37 +0800

    fix: 修复 MD5 函数重复 digest 的问题

--- packages/helpers/src/md5.ts
+++ packages/helpers/src/md5.ts
@@ -3,18 +3,23 @@ import * as fs from 'fs';
 
 function getMD5(file: string, fileSystem = fs): Promise<string> {
   const fileStream = fileSystem.createReadStream(file);
-
   const hash = createHash('md5');
-
+  let result = '';
   return new Promise((resolve, reject) => {
     fileStream.on('readable', () => {
+      
       let data: string;
 
       while (data = fileStream.read()) {
         hash.update(data);
       }
 
-      resolve(hash.digest('hex'));
+      // TODO: Why the readable event will be emitted twice?
+      if (result) {
+        return;
+      }
+
+      resolve(result = hash.digest('hex'));
     });
 
     fileStream.once('error', err => {
