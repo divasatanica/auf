68e5d25aebcdd92d9c63845a7b1870be18d9f3b7
divasatanica <coma.desperado@gmail.com>
2022-03-18 18:10:33 +0800

    refactor: 重构 IMiddlewareAbility 接口中的 next 类型

--- packages/core/src/server/middlewares.ts
+++ packages/core/src/server/middlewares.ts
@@ -3,27 +3,28 @@ import { IContext, IMiddleWare } from '@vergiss/auf-typing';
 import { IMiddlewareAbility } from './interface';
 
 class Middlewares {
-  public middlewares: Function[];
-  constructor (middlewares: Function[]) {
+  private middlewares: Array<IMiddleWare>;
+  private wrappedMiddlewares: Array<(fn?: IMiddleWare) => void>;
+  constructor (middlewares: IMiddleWare[]) {
     this.middlewares = middlewares
   }
 
-  applyMiddlewares(): (ctx: IContext) => Promise<unknown> {
-    this.middlewares = this.middlewares.map(fn => {
+  applyMiddlewares(): (ctx: IContext) => void {
+    this.wrappedMiddlewares = this.middlewares.map(fn => {
       return function (next = (() => Promise.resolve())) {
         return async function (ctx: IContext) {
           await fn(ctx, next);
         }
       }
-    })
-    const [lastActionGenerator, ...restActionGenerators] = this.middlewares.reverse();
+    });
+    const [lastActionGenerator, ...restActionGenerators] = this.wrappedMiddlewares.reverse();
 
     return compose(restActionGenerators)(lastActionGenerator());
   }
 }
 
 export class MiddlewaresAbility implements IMiddlewareAbility {
-  next: (ctx: IContext) => Promise<unknown>;
+  next: (ctx: IContext) => void;
   private middleware: Middlewares;
   applyMiddleware(middlewares: IMiddleWare[]): void {
     this.middleware = new Middlewares(middlewares);
